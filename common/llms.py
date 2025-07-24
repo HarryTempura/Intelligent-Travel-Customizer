00417d63a264e6943ac3d14af702c5bcb25a8b50
LiuJM <liujiaming6656@gmail.com>
2025-07-24 21:45:28 +0800

    新增 QWEN3_TURBO 模型

--- common/llms.py
+++ common/llms.py
@@ -1,4 +1,6 @@
+from dotenv import dotenv_values
 from langchain_ollama import ChatOllama
+from langchain_openai import ChatOpenAI
 
 from common.configs import configs
 
@@ -16,4 +18,12 @@ OLLAMA_QWEN3_06B = ChatOllama(
     model=configs['ollama']['qwen3_06b'],
     temperature=configs['ollama']['temperature'],
     num_predict=configs['ollama']['num_predict']
+)
+
+QWEN3_TURBO = ChatOpenAI(
+    model=configs['qwen']['turbo'],
+    temperature=configs['qwen']['temperature'],
+    max_retries=configs['qwen']['max_retries'],
+    base_url=configs['qwen']['base_url'],
+    api_key=dotenv_values('.env')['DASHSCOPE_API_KEY']
 )
